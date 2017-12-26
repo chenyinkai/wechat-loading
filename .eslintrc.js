c1836f33f457f6b5072e6bc4292c67687ace61b2
chenyinkai <287696322@qq.com>
2017-12-26 11:43:43 +0800

    use eslint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true,
+        es6: true
+    },
+    extends: "standard",
+    rules: {
+        "no-tabs": "off",
+        "indent": ["error", "tab"]
+    }
+}
